de377658f865325994daa7149571ff78ceb4ab2e
Leoltron <leoole5@gmail.com>
2022-04-08 03:54:21 +0500

    Slightly improved memory usage of AsyncifiableMethodsProvider

--- CascadeAsyncifier/Asyncifier/AsyncifiableMethodsProvider.cs
+++ CascadeAsyncifier/Asyncifier/AsyncifiableMethodsProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using CascadeAsyncifier.Extensions;
 using CascadeAsyncifier.Helpers;
@@ -6,6 +7,7 @@ using Microsoft.CodeAnalysis;
 
 namespace CascadeAsyncifier.Asyncifier
 {
+    [SuppressMessage("ReSharper", "ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator")]
     public class AsyncifiableMethodsProvider
     {
         private readonly Compilation compilation;
@@ -32,31 +34,33 @@ namespace CascadeAsyncifier.Asyncifier
                 typesWithAsyncMethodPairs.GetOrDefault(typeSymbol.GetFullName(), new List<(string, string)>())
                                          .ToDictionary(p => p.Item1, p => p.Item2);
             
-            var members = typeSymbol.GetMembers().OfType<IMethodSymbol>().ToList();
-            var (asyncMembers, syncMembers) = members.SplitByFilter(m => m.IsAsync || m.Name.EndsWith("Async"));
-            foreach (var member in syncMembers)
+            var methods = typeSymbol.GetMembers().OfType<IMethodSymbol>().ToList();
+            foreach (var method in methods)
             {
-                if (syncAsyncMethodPairs.TryGetValue(member.Name, out var asyncName))
+                if (method.IsAsync || method.Name.EndsWith("Async"))
+                    continue;
+                
+                if (syncAsyncMethodPairs.TryGetValue(method.Name, out var asyncName))
                 {
-                    foreach (var asyncMethod in members.Where(m => m.Name == asyncName))
+                    foreach (var asyncMethod in methods.Where(m => m.Name == asyncName))
                     {
-                        if (asyncMethod.SymbolEquals(member) || !methodCompareHelper.IsAsyncVersionOf(member, asyncMethod, true))
+                        if (asyncMethod.SymbolEquals(method) || !methodCompareHelper.IsAsyncVersionOf(method, asyncMethod, true))
                             continue;
                         
-                        var pair = new SyncAsyncMethodSymbolPair(member, asyncMethod);
+                        var pair = new SyncAsyncMethodSymbolPair(method, asyncMethod);
                         yield return pair;
                         break;
                     } 
                     continue;
                 }
 
-                var matchingAsyncName = member.Name + "Async";
-                var matchingAsyncMethod = asyncMembers
-                                         .FirstOrDefault(a =>  a.Name == matchingAsyncName && methodCompareHelper.IsAsyncVersionOf(member, a));
+                var matchingAsyncMethod = typeSymbol.GetMembers(method.Name + "Async")
+                    .OfType<IMethodSymbol>()
+                    .FirstOrDefault(otherMethod => methodCompareHelper.IsAsyncVersionOf(method, otherMethod));
 
                 if (matchingAsyncMethod != null)
                 {
-                    var pair = new SyncAsyncMethodSymbolPair(member, matchingAsyncMethod);
+                    var pair = new SyncAsyncMethodSymbolPair(method, matchingAsyncMethod);
                     yield return pair;
                 }
             }
